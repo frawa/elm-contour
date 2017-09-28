9527f5fbf2305988e55d3cd89a0ad55c687395f0
Frank Wagner <frank.wagner.13@gmail.com>
2017-09-28 22:51:10 +0200

    marching squares: classify squares

--- src/elm/Contour.elm
+++ src/elm/Contour.elm
@@ -1,8 +1,9 @@
 module Contour exposing (..)
 
-import Array exposing (Array, get, fromList)
+import Array exposing (Array, get, fromList, map, foldl)
 import Maybe exposing (withDefault)
 import List exposing (map, range)
+import Bitwise exposing (or, shiftLeftBy)
 
 
 type alias Position =
@@ -20,10 +21,18 @@ type alias Grid =
     }
 
 
-type alias GridFunction =
+type alias Squares =
+    Grid
+
+
+type alias GridValues a =
     { grid : Grid
-    , values : Array Float
+    , values : Array a
     }
+
+
+type alias GridFunction =
+    GridValues Float
 
 
 stepSize : Grid -> Point
@@ -86,15 +95,94 @@ position grid index =
 
 listPositions : Grid -> List Position
 listPositions grid =
-    map (position grid) <| range 0 ((gridSize grid) - 1)
+    List.map (position grid) <| range 0 ((gridSize grid) - 1)
 
 
 valueAt : GridFunction -> Position -> Float
 valueAt gfun coor =
     withDefault 0 <|
-        get (index (.grid gfun) coor) (.values gfun)
+        value gfun (index (.grid gfun) coor)
+
+
+value : GridValues a -> Int -> Maybe a
+value gfun index =
+    get index (.values gfun)
 
 
 gridFunction : Grid -> (Point -> Float) -> GridFunction
 gridFunction grid f =
-    { grid = grid, values = fromList <| map (f << (point grid)) (listPositions grid) }
+    { grid = grid, values = fromList <| List.map (f << (point grid)) (listPositions grid) }
+
+
+markLevel : GridFunction -> Float -> GridValues Int
+markLevel gfun level =
+    { gfun
+        | values =
+            Array.map
+                (\more ->
+                    if more then
+                        1
+                    else
+                        0
+                )
+            <|
+                Array.map ((<) level) gfun.values
+    }
+
+
+squares : Grid -> Squares
+squares grid =
+    { grid | steps = grid.steps - 1 }
+
+
+squareCornerIndex : Squares -> Int -> Int
+squareCornerIndex squares i =
+    let
+        grid =
+            { squares | steps = squares.steps + 1 }
+    in
+        index grid <| position squares i
+
+
+corners : Squares -> Int -> List Int
+corners squares index =
+    let
+        n =
+            dimensionSize squares + 1
+
+        corner =
+            squareCornerIndex squares index
+    in
+        [ corner, corner + 1, corner + n + 1, corner + n ]
+
+
+classify : List Int -> Int
+classify marked =
+    List.foldl
+        (\mark acc ->
+            or mark (shiftLeftBy 1 acc)
+        )
+        0
+        marked
+
+
+classifySquares : GridFunction -> Float -> GridValues Int
+classifySquares gfun level =
+    let
+        marked =
+            markLevel gfun level
+
+        squares_ =
+                squares <|
+                    .grid marked
+    in
+        { marked
+            | grid = squares_
+            , values =
+                listPositions squares_
+                    |> List.map (index squares_)
+                    |> List.map (corners squares_)
+                    |> List.map (\corners -> List.map (value marked) corners |> List.map (withDefault 0))
+                    |> List.map classify
+                    |> fromList
+        }
--- tests/ContourTest.elm
+++ tests/ContourTest.elm
@@ -108,5 +108,136 @@ suite =
                             gridFunction grid f
                     in
                         Expect.equal (f (point grid pos)) (valueAt gfun pos)
+            , fuzz (tuple ( intRange 0 10, intRange 0 10 )) "simple function by index" <|
+                \pos ->
+                    let
+                        grid =
+                            { min = ( 0, 0 ), max = ( 1, 1 ), steps = 10 }
+
+                        f =
+                            \( x, y ) -> x * y
+
+                        gfun =
+                            gridFunction grid f
+                    in
+                        Expect.equal (f (point grid pos)) (value gfun (index grid pos) |> Maybe.withDefault 0)
+            ]
+        , describe "marching squares"
+            [ test "mark level" <|
+                \_ ->
+                    let
+                        grid =
+                            { min = ( 0, 0 ), max = ( 1, 1 ), steps = 2 }
+
+                        gfun =
+                            { grid = grid
+                            , values =
+                                Array.fromList
+                                    [ 0
+                                    , 0
+                                    , 0
+                                    , 0
+                                    , 9
+                                    , 0
+                                    , 0
+                                    , 0
+                                    , 0
+                                    ]
+                            }
+                    in
+                        Expect.equal
+                            (Array.fromList
+                                [ 0
+                                , 0
+                                , 0
+                                , 0
+                                , 1
+                                , 0
+                                , 0
+                                , 0
+                                , 0
+                                ]
+                            )
+                            (.values <| markLevel gfun 5)
+            , test "squares" <|
+                \_ ->
+                    let
+                        grid =
+                            { min = ( 0, 0 ), max = ( 1, 1 ), steps = 2 }
+                    in
+                        Expect.equal { grid | steps = 1 } (squares grid)
+            , test "a squares corner index in the grid" <|
+                \_ ->
+                    let
+                        grid =
+                            { min = ( 0, 0 ), max = ( 1, 1 ), steps = 3 }
+
+                        boxes =
+                            squares grid
+                    in
+                        Expect.equal
+                            [ 0
+                            , 4
+                            ]
+                            [ squareCornerIndex boxes 0
+                            , squareCornerIndex boxes 3
+                            ]
+            , test "corners" <|
+                \_ ->
+                    let
+                        grid =
+                            { min = ( 0, 0 ), max = ( 1, 1 ), steps = 2 }
+
+                        boxes =
+                            squares grid
+                    in
+                        Expect.equal
+                            [ [ 0, 1, 4, 3 ]
+                            , [ 4, 5, 8, 7 ]
+                            ]
+                            [ corners boxes 0
+                            , corners boxes 3
+                            ]
+            , test "classify one square" <|
+                \_ ->
+                    Expect.equal [ 0, 8, 4, 2, 1, 15 ]
+                        [ classify [ 0, 0, 0, 0 ]
+                        , classify [ 1, 0, 0, 0 ]
+                        , classify [ 0, 1, 0, 0 ]
+                        , classify [ 0, 0, 1, 0 ]
+                        , classify [ 0, 0, 0, 1 ]
+                        , classify [ 1, 1, 1, 1 ]
+                        ]
+            , test "classify squares" <|
+                \_ ->
+                    let
+                        grid =
+                            { min = ( 0, 0 ), max = ( 1, 1 ), steps = 2 }
+
+                        gfun =
+                            { grid = grid
+                            , values =
+                                Array.fromList
+                                    [ 0
+                                    , 0
+                                    , 0
+                                    , 0
+                                    , 9
+                                    , 0
+                                    , 0
+                                    , 0
+                                    , 0
+                                    ]
+                            }
+                    in
+                        Expect.equal
+                            (Array.fromList
+                                [ 2
+                                , 1
+                                , 4
+                                , 8
+                                ]
+                            )
+                            (.values <| classifySquares gfun 5)
             ]
         ]
